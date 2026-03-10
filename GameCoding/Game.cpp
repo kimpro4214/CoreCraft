1883d713a4fad224d3d22abadf275e737fc1265e
kimpro4214 <qnfghk555@naver.com>
2026-03-10 14:36:31 +0900

    feat: DirectX11 삼각형 렌더링 파이프라인 구현 및 빌드 오류 수정
    - Game 클래스에 Geometry, VS, PS, InputLayout 생성 함수 추가
    - 정점 버퍼(VertexBuffer) 및 셰이더(Default.hlsl) 연동 구현
    - Render()에 IA/VS/PS 스테이지 설정 및 Draw 호출 추가
    - Struct.h에 Vertex 구조체(Vec3 position, Color color) 정의
    - pch.h에 누락된 <string> 헤더 추가
    - Game.cpp에 PCH(Use) 설정 명시하여 wstring/string 인식 오류 수정

--- GameCoding/Game.cpp
+++ GameCoding/Game.cpp
@@ -17,10 +17,14 @@ void Game::Init(HWND hwnd)
 	_width = GWinSizeX;
 	_height = GWinSizeY;
 
-	// TODO
 	CreateDeviceAndSwapChain();
 	CreateRenderTargetView();
 	SetViewport();
+
+	CreateGeometry();
+	CreateVS();
+	CreateInputLayout();
+	CreatePS();
 }
 
 void Game::Update()
@@ -32,9 +36,26 @@ void Game::Render()
 {
 	RenderBegin();
 
-	// IA - VS - RS - PS - OM
 	{
+		uint32 stride = sizeof(Vertex);
+		uint32 offset = 0;
 
+		// IA
+		_deviceContext->IASetVertexBuffers(0, 1, _vertexBuffer.GetAddressOf(), &stride, &offset);
+		_deviceContext->IASetInputLayout(_inputLayout.Get());
+		_deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+		// VS
+		_deviceContext->VSSetShader(_vertexShader.Get(), nullptr, 0);
+
+		// RS
+
+		// PS
+		_deviceContext->PSSetShader(_pixelShader.Get(), nullptr, 0);
+
+		// OM
+
+		_deviceContext->Draw(_vertices.size(), 0);
 	}
 
 	RenderEnd();
@@ -112,4 +133,78 @@ void Game::SetViewport()
 	_viewport.Height = static_cast<float>(_height);
 	_viewport.MinDepth = 0.f;
 	_viewport.MaxDepth = 1.f;
+}
+
+void Game::CreateGeometry()
+{
+	// VertexData
+	{
+		_vertices.resize(3);
+
+		_vertices[0].position = Vec3(-0.5f, -0.5f, 0.f);
+		_vertices[0].color = Color(1.f, 0.f, 0.f, 1.f);
+		_vertices[1].position = Vec3(0.f, 0.5f, 0.f);
+		_vertices[1].color = Color(0.f, 1.f, 0.f, 1.f);
+		_vertices[2].position = Vec3(0.5f, -0.5f, 0.f);
+		_vertices[2].color = Color(0.f, 0.f, 1.f, 1.f);
+	}
+
+	// VertexBuffer
+	{
+		D3D11_BUFFER_DESC desc;
+		ZeroMemory(&desc, sizeof(desc));
+		desc.Usage = D3D11_USAGE_IMMUTABLE;
+		desc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		desc.ByteWidth = (uint32)(sizeof(Vertex) * _vertices.size());
+
+		D3D11_SUBRESOURCE_DATA data;
+		ZeroMemory(&data, sizeof(data));
+		data.pSysMem = _vertices.data();
+
+		_device->CreateBuffer(&desc, &data, _vertexBuffer.GetAddressOf());
+	}
+}
+
+void Game::CreateInputLayout()
+{
+	D3D11_INPUT_ELEMENT_DESC layout[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	};
+
+	const int32 count = sizeof(layout) / sizeof(D3D11_INPUT_ELEMENT_DESC);
+	_device->CreateInputLayout(layout, count, _vsBlob->GetBufferPointer(), _vsBlob->GetBufferSize(), _inputLayout.GetAddressOf());
+}
+
+void Game::CreateVS()
+{
+	LoadShaderFromFile(L"Default.hlsl", "VS", "vs_5_0", _vsBlob);
+	HRESULT hr = _device->CreateVertexShader(_vsBlob->GetBufferPointer(), _vsBlob->GetBufferSize(), nullptr, _vertexShader.GetAddressOf());
+	CHECK(hr);
+}
+
+void Game::CreatePS()
+{
+	LoadShaderFromFile(L"Default.hlsl", "PS", "ps_5_0", _psBlob);
+	HRESULT hr = _device->CreatePixelShader(_psBlob->GetBufferPointer(), _psBlob->GetBufferSize(), nullptr, _pixelShader.GetAddressOf());
+	CHECK(hr);
+}
+
+void Game::LoadShaderFromFile(const wstring& path, const string& name, const string& version, ComPtr<ID3DBlob>& blob)
+{
+	const uint32 compileFlag = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+
+	HRESULT hr = ::D3DCompileFromFile(
+		path.c_str(),
+		nullptr,
+		D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		name.c_str(),
+		version.c_str(),
+		compileFlag,
+		0,
+		blob.GetAddressOf(),
+		nullptr);
+
+	CHECK(hr);
 }
