0cb2ee9c8a7ca2f0e4917dffe8c8ab0f5e0bc524
kimpro4214 <qnfghk555@naver.com>
2026-03-24 16:37:31 +0900

    refactor: Game 클래스를 GameObject 기반으로 리팩토링

--- GameCoding/Game.h
+++ GameCoding/Game.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Graphics.h"
+#include "GameObject.h"
 
 class Game
 {
@@ -14,59 +15,10 @@ public:
 	void Render();
 
 private:
-
-private:
-	void CreateGeometry();
-	void CreateInputLayout();
-	void CreateVS();
-	void CreatePS();
-
-	void CreateRasterizerState();
-	void CreateSamplerState();
-	void CreateBlendState();
-	void CreateSRV();
-
-	void CreateConstantBuffer();
-
-	void LoadShaderFromFile(const wstring& path, const string& name, const string& version, ComPtr<ID3DBlob>& blob);
-
-private:
 	HWND _hwnd;
 
 	shared_ptr<Graphics> _graphics;
-
-private:
-	// Geometry
-	shared_ptr<Geometry<VertexTextureData>> _geometry;
-	shared_ptr<VertexBuffer> _vertexBuffer;
-	shared_ptr<IndexBuffer> _indexBuffer;
-	shared_ptr<InputLayout> _inputLayout;
-
-	// VS
-	ComPtr<ID3D11VertexShader> _vertexShader = nullptr;
-	ComPtr<ID3DBlob> _vsBlob = nullptr;
-
-	// RS
-	ComPtr<ID3D11RasterizerState> _rasterizerState = nullptr;
-
-	// PS
-	ComPtr<ID3D11PixelShader> _pixelShader = nullptr;
-	ComPtr<ID3DBlob> _psBlob = nullptr;
-
-	// SRV
-	ComPtr<ID3D11ShaderResourceView> _shaderResourveView = nullptr;
-	ComPtr<ID3D11ShaderResourceView> _shaderResourveView2 = nullptr;
-
-	ComPtr<ID3D11SamplerState> _samplerState = nullptr;
-	ComPtr<ID3D11BlendState> _blendState = nullptr;
-	// [ CPU<->RAM ] [GPU<->VRAM]
-private:
-	// SRT
-	TransformData _transformData;
-	ComPtr<ID3D11Buffer> _constantBuffer;
-
-	Vec3 _localPosition = {0.f, 0.f, 0.f};
-	Vec3 _localRotation = {0.f, 0.f, 0.f};
-	Vec3 _localScale = {1.f, 1.f, 1.f};
+	shared_ptr<Pipeline> _pipeline;
+	shared_ptr<GameObject> _gameObject;
 };
 
