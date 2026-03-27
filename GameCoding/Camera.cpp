552379a3cb041f5a062e198532b419f6db1082ba
kimpro4214 <qnfghk555@naver.com>
2026-03-27 20:27:48 +0900

    feat: Camera, MeshRenderer 컴포넌트 추가
    - Camera: Orthographic/Perspective 투영 지원, View/Projection 행렬 계산
    - MeshRenderer: 메시 렌더링 컴포넌트 (Geometry, VertexBuffer, Material 연동)
    - Camera 정적 멤버 S_MatView, S_MatProjection으로 전역 카메라 행렬 공유

---
+++ GameCoding/Camera.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "Camera.h"
+
+Matrix Camera::S_MatView = Matrix::Identity;
+Matrix Camera::S_MatProjection = Matrix::Identity;
+
+Camera::Camera() : Super(ComponentType::Camera)
+{
+
+}
+
+Camera::~Camera()
+{
+
+}
+
+void Camera::Update()
+{
+	UpdateMatrix();
+}
+
+void Camera::UpdateMatrix()
+{
+	Vec3 eyePosition = GetTransform()->GetPosition();
+	Vec3 focusPosition = eyePosition + GetTransform()->GetLook();
+	Vec3 upDirection = GetTransform()->GetUp();
+	S_MatView = ::XMMatrixLookAtLH(eyePosition, focusPosition, upDirection);
+
+	//S_MatView = GetTransform()->GetWorldMatrix().Invert();
+
+	if (_type == ProjectionType::Perspective)
+		S_MatProjection = ::XMMatrixPerspectiveFovLH(XM_PI / 4.f, 800.f / 600.f, 1.f, 100.f);
+	else
+		S_MatProjection = ::XMMatrixOrthographicLH(8, 6, 0.f, 1.f);
+}
