579e9be1087f5b343e0aa07365fdd527fa401f09
kimpro4214 <qnfghk555@naver.com>
2026-04-10 04:19:34 +0900

    feat: Client 데모 씬 및 CameraScript 추가 (02~09 Demo)

--- Client/Main.cpp
+++ Client/Main.cpp
@@ -2,6 +2,14 @@
 #include "Main.h"
 #include "Engine/Game.h"
 #include "01. TriangleDemo.h"
+#include "02. QuadDemo.h"
+#include "03. ConstBufferDemo.h"
+#include "04. CameraDemo.h"
+#include "05. TextureDemo.h"
+#include "06. SamplerDemo.h"
+#include "07. HeightMapDemo.h"
+#include "08. NormalDemo.h"
+#include "09. MeshDemo.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
@@ -13,7 +21,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	desc.width = 800;
 	desc.height = 600;
 	desc.clearColor = Color(0.5f, 0.5f, 0.5f, 0.5f);
-	desc.app = make_shared<TriangleDemo>();
+	desc.app = make_shared<MeshDemo>();
 
 	GAME->Run(desc);
 
