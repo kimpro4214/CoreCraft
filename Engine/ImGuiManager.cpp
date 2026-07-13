98805b9d309a6e15fb6f2001f6ad3687fb47b803
kimpro4214 <qnfghk555@naver.com>
2026-07-13 18:15:33 +0900

    chore: 에디터 UI 라이브러리 구성

--- Engine/ImGuiManager.cpp
+++ Engine/ImGuiManager.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "ImGuiManager.h"
 
 
@@ -9,6 +9,7 @@ void ImGuiManager::Init()
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
 	// Setup Dear ImGui style
 	ImGui::StyleColorsDark();
@@ -31,4 +32,11 @@ void ImGuiManager::Render()
 	// Rendering
 	ImGui::Render();
 	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+}
+
+void ImGuiManager::Shutdown()
+{
+	ImGui_ImplDX11_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
 }
