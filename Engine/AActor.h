076a9866ac3f2ff57561f7513288219948891584
kimpro4214 <qnfghk555@naver.com>
2026-09-21 16:22:06 +0900

    feat: AActor 부모-자식 관계와 컴포넌트 추가/삭제 추가
    AttachToActor/DetachFromActor/GetParentActor/GetAttachedActors는
    루트 컴포넌트끼리 USceneComponent::AttachToComponent를 호출하는
    방식(언리얼 정통 방식)으로 구현한다. GetAttachedActors는 루트
    컴포넌트의 자식 중 그 자식의 루트 컴포넌트인 것만 자식 액터로
    취급해 액터 내부의 서브 씬 컴포넌트 attachment와 구분한다.
    
    RemoveComponent를 추가하고, 템플릿 AddComponent<T>()의 등록 로직을
    RegisterComponent()로 뽑아 비템플릿 AddComponent(shared_ptr) 오버로드와
    공유한다(런타임에 타입 이름으로 컴포넌트를 만드는 레지스트리에서 필요).
    루트 컴포넌트를 제거할 때는 그 자식들을 먼저 detach해 댕글링 부모를
    참조하는 컴포넌트가 남지 않게 한다.

--- Engine/AActor.h
+++ Engine/AActor.h
@@ -22,19 +22,17 @@ public:
 		static_assert(is_base_of_v<UActorComponent, T>, "T must derive from UActorComponent");
 
 		shared_ptr<T> component = make_shared<T>();
-		component->SetOwner(static_pointer_cast<AActor>(shared_from_this()));
-		_components.push_back(component);
-
-		if (!_rootComponent)
-		{
-			shared_ptr<USceneComponent> sceneComponent = dynamic_pointer_cast<USceneComponent>(component);
-			if (sceneComponent)
-				_rootComponent = sceneComponent;
-		}
-
-		component->OnRegister();
+		RegisterComponent(component);
 		return component;
 	}
+
+	shared_ptr<UActorComponent> AddComponent(const shared_ptr<UActorComponent>& component)
+	{
+		RegisterComponent(component);
+		return component;
+	}
+
+	bool RemoveComponent(const shared_ptr<UActorComponent>& component);
 
 	template<typename T>
 	shared_ptr<T> GetComponentByClass() const
@@ -53,11 +51,19 @@ public:
 
 	const vector<shared_ptr<UActorComponent>>& GetComponents() const { return _components; }
 
+	bool AttachToActor(const shared_ptr<AActor>& parent);
+	void DetachFromActor();
+	shared_ptr<AActor> GetParentActor() const;
+	vector<shared_ptr<AActor>> GetAttachedActors() const;
+
 	shared_ptr<UWorld> GetWorld() const { return _world.lock(); }
 	void SetWorld(const shared_ptr<UWorld>& world) { _world = world; }
 
 	bool IsActive() const { return _active; }
 	void SetActive(bool active) { _active = active; }
+
+private:
+	void RegisterComponent(const shared_ptr<UActorComponent>& component);
 
 private:
 	shared_ptr<USceneComponent> _rootComponent;
